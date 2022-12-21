ee46e22b9a620888e2558492832f3157b0a456c5
MavicaSoftwareDev <mohamadamgad09@gmail.com>
2022-12-21 08:27:02 +0200

    start server impl

--- Payment_app/Payment_app/Payment_app/Server/server.c
+++ Payment_app/Payment_app/Payment_app/Server/server.c
@@ -4,5 +4,19 @@
 #include "server.h"
 
 
-ST_accountsDB_t account1={10000.0,BLOCKED,"8989374615436851"};
-ST_accountsDB_t accountsDB[255]={account1};
+  ST_accountsDB_t account1={10000.0, BLOCKED, "8989374615436851"};
+ST_accountsDB_t account2={10000.0,BLOCKED,"8989374615436851"};
+ST_accountsDB_t account3={10000.0,BLOCKED,"8989374615436851"};
+ST_accountsDB_t account4={10000.0,BLOCKED,"8989374615436851"};
+
+struct ST_accountsDB_t accountsDB[255]={
+        {10000.0,BLOCKED,"8989374615436851"}
+};
+
+
+
+struct ST_transaction_t transactionsDB[255];
+
+int main(){
+    printf("\n\n server start \n\n");
+}
--- Payment_app/Payment_app/Payment_app/Server/server.h
+++ Payment_app/Payment_app/Payment_app/Server/server.h
@@ -8,13 +8,13 @@ typedef unsigned char uint32_t ;
 typedef enum EN_transState_t
 {
      APPROVED, DECLINED_INSUFFECIENT_FUND, DECLINED_STOLEN_CARD, FRAUD_CARD, INTERNAL_SERVER_ERROR
-}EN_transStat_t;
+}EN_transState_t;
 
 typedef struct ST_transaction_t
 {
      ST_cardData_t cardHolderData;
      ST_terminalData_t terminalData;
-     EN_transState_t transState;
+     EN_transState_t  transState;
      uint32_t transactionSequenceNumber;
 }ST_transaction;
 
